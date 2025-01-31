e76dd216ed93bf8ba45e4aef441561a7e041597a
nanda <nandamonsterking@gmail.com>
2025-01-31 15:44:57 +0530

    added css major assignment

---
+++ assets/js/side-bar-handle.js
@@ -0,0 +1,12 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+        const sidebar = document.querySelector('.sidebar');
+
+        hamburgerMenu.addEventListener('click', () => {
+                    sidebar.classList.toggle('active');
+        });
+
+        document.addEventListener("click", (event) => {
+    if (!sidebar.contains(event.target) && !hamburger.contains(event.target)) {
+        sidebar.style.left = "-250px";
+    }
+});
